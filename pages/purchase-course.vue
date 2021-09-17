6ce570a180e55df6ad69614db7c11967370b1068
Daniel Adeyemo <adeyemodaniel10@gmail.com>
2021-09-17 13:58:20 +0100

    change to heroku

--- pages/purchase-course.vue
+++ pages/purchase-course.vue
@@ -137,15 +137,8 @@ export default {
   methods: {
     submit() {
       axios
-        .post('https://api.birdsend.co/v1/contacts', {
-          headers: {
-            Authorization:
-              'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjMyZGNlMjVmOTFmMTgwYmRmMmQ5YjVlOTliN2JmNDNjMzg3MDQ5MWUzZTIxMGEwYzVmNWQ0YTNiZTYzMjQ4YWU2MmE1NDIwNjlkZGM4NTJjIn0.eyJhdWQiOiI1IiwianRpIjoiMzJkY2UyNWY5MWYxODBiZGYyZDliNWU5OWI3YmY0M2MzODcwNDkxZTNlMjEwYTBjNWY1ZDRhM2JlNjMyNDhhZTYyYTU0MjA2OWRkYzg1MmMiLCJpYXQiOjE2MzE2MzE5NTEsIm5iZiI6MTYzMTYzMTk1MSwiZXhwIjoxNjYzMTY3OTUxLCJzdWIiOiIyMzQxIiwic2NvcGVzIjpbIndyaXRlIl19.f18VUBnGVU_2UPKl8ZmFUsBLu6dSp0bPjwGmgOUpzntDu6l6XZ69iSX6a8gWzngDWxpTRrMEC6qCPCwEdlGcZabh5N5xhZrW7640ohyMpaK2YIHtRlhSHgS-wlXsoKWXXd84wWWHAKy3MD3nvBGq-WLIuuXZLAw9wb7A9IBfUarxuV2_ecP_BUCqffxiqfGVZIDZPN1HVWsOk0UWrm4MiZZgpa0T4LkfpywCSXz40L6B9Kd_vQXtNKsAIQCS9LrkZbR-J3TbeqE9c8I1ogNjR-bN_UfMyovBySwzGlf-AnIiVPsh99gM84dJZz3Qa2lfyuJxCOGy8tqvjszFPC779dfI80Al8IsSahDflz_q8-DMQ1U06vvxRsmNsRMdUvZqxoPA5oj2aYNVLWf7Cyxo_M6S872pOYxNf4HbTe7jgLU5YKoUtI7_e6BWj94KRfFtIi6wOJjZ5rt4X8AYBkE6zOcBTObcG5tN8cFDCMIpz9_KkpUL5I0uJEWJzy2qCJCx-CgHILUsxaRq2BfZTcGTvVB1VgidGrdBfIUblnUA31kyZSwrLuNkih6v1tpyT9xx2NsmbkHw8e2UYM1CYNeN08IU7PEOHwSr845Hg3AL0zEuaLLe8Kw5Jot-FXh47Ttml922sJDabeZpFVsxN7Uduas8VuKawrLt11-g-tCo2Xc',
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          },
+        .post('https://helpmate-be.kerokuapp.com/jet-purchase', {
           email: this.email,
-          tags: ['purchase'],
         })
         .then(() => {
           console.log('Email submitted')

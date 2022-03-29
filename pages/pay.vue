1c64662515e3616c7ed5409f97a4863f773661e9
Daniel Adeyemo <adeyemodaniel10@gmail.com>
2022-03-29 16:08:46 +0100

    push /pay

---
+++ pages/pay.vue
@@ -0,0 +1,190 @@
+<template>
+  <div class="dm">
+    <DeadNav />
+    <div class="xl:px-44 sm:px-20 px-8 pt-28 pb-12 lg:pt-52 bg-green relative">
+      <div
+        class="text-blue flex flex-col lg:flex-row text-center justify-between"
+      >
+        <div class="mx-auto text-center">
+          <h1
+            class="
+              mt-4
+              xl:mt-6
+              text-3xl
+              sm:text-5xl
+              xl:text-6xl
+              font-bold
+              sm:w-8/12
+              lg:w-7/12
+              mx-auto
+              space
+              tracking-tight
+            "
+          >
+            30-Day Mentorship with Joe
+          </h1>
+          <h2
+            class="
+              text-center
+              mx-auto
+              mt-6
+              font-medium
+              text-base
+              sm:text-xl
+              lg:text-xl
+              xl:text-2xl
+              w-11/12
+              sm:w-8/12
+              lg:w-7/12
+              text-dark
+            "
+          >
+            Complete your payment below to gain access to my premium mentorship
+            circle.
+          </h2>
+        </div>
+      </div>
+    </div>
+
+    <!-- @submit.prevent -->
+    <div class="xl:mt-20 mx-auto mt-2 sm:w-10/12 lg:w-9/12 xl:w-8/12">
+      <form
+        action=""
+        method="post"
+        class="mt-6 flex flex-col items-center space-y-6 avenir-regular px-5"
+      >
+        <input type="hidden" name="meta_id" value="10495" />
+        <input type="hidden" name="meta_user_id" value="2341" />
+        <input
+          type="text"
+          v-model="name"
+          name="name"
+          required
+          class="
+            border-2 border-gray-300
+            rounded-xl
+            lg:text-lg
+            xl:text-xl
+            py-2
+            px-5
+            xl:w-7/12
+            lg:w-8/12
+            sm:w-9/12
+            w-full
+            focus:outline-none
+          "
+          placeholder="Name*"
+        />
+        <input
+          type="email"
+          v-model="email"
+          name="email"
+          required
+          class="
+            border-2 border-gray-300
+            rounded-xl
+            lg:text-lg
+            xl:text-xl
+            py-2
+            px-5
+            xl:w-7/12
+            lg:w-8/12
+            sm:w-9/12
+            w-full
+            focus:outline-none
+          "
+          placeholder="Email address*"
+        />
+        <div class="mt-8 text-center mx-auto w-full flex justify-center">
+          <button type="submit" class="cta uppercase" @click="payViaService">
+            complete payment
+          </button>
+        </div>
+      </form>
+    </div>
+
+    <Footer />
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      email: '',
+      name: '',
+      paymentData: {
+        tx_ref: this.generateReference(),
+        amount: 100,
+        currency: 'USD',
+        payment_options: 'card',
+        redirect_url: '',
+        meta: {
+          counsumer_id: '7898',
+          consumer_mac: 'kjs9s8ss7dd',
+        },
+        customer: {
+          name: 'Demo Customer  Name',
+          email: 'customer@mail.com',
+          phone_number: '081845***044',
+        },
+        customizations: {
+          title: 'Joe Elite',
+          description: 'Flutterwave Test',
+          logo: 'https://flutterwave.com/images/logo-colored.svg',
+        },
+        callback: this.makePaymentCallback,
+        onclose: this.closedPaymentModal,
+      },
+      loadingMore: false,
+      currentPage: 1,
+
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+
+    payViaService() {
+      try {
+        this.payWithFlutterwave(this.paymentData);
+      } catch (e) {
+        if (e.message === 'window.FlutterwaveCheckout is not a function') {
+          this.showNotification('error', 'Network Failure');
+        }
+      }
+    },
+    makePaymentCallback(response) {
+      if (response.status === 'successful') {
+        this.closePaymentModal(10);
+      }
+    },
+    generateReference() {
+      let date = new Date();
+      return date.getTime().toString();
+    },
+    submit() {
+      axios
+        .post('https://www.google.com/', {
+          email: this.email,
+        })
+        .then(() => {
+
+        })
+        .catch((error) => {
+          console.log('error: ', error)
+        })
+    },
+  },
+  head() {
+    return {
+      title:
+        'Learn How to Make Consistent Profits Trading :: Joe Elite TradeHub',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
